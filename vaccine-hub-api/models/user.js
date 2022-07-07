feac33f3ee686d0219c9f0a0904dfb2b58a28b9c
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-07-07 11:43:05 -0700

    done ish

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -7,11 +7,40 @@ const { BadRequestError } = require("../utils/errors");
 const { UnauthorizedError } = require("../utils/errors");
 
 class User {
+  static async makePubUser() {
+    return {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      location: user.location,
+      date: user.date,
+    };
+  }
+
   static async login(creds) {
     //enter creds, if missing throw err
+    const reqFields = ["email", "password"];
+    reqFields.forEach((field) => {
+      if (!creds.hasOwnProperty(field)) {
+        throw new BadRequestError(`missing ${field} in req body`); //test on posty by entering nothing -->/auth/login
+      }
+    });
+
+    const user = await User.fetchUserByEmail(creds.email);
+    if (user) {
+      throw new BadRequestError(`Alr signed ${creds.email} up`);
+    }
     //lookup user in db, if found compare password
+    if (user) {
+      const isvalid = await bcrypt.compare(creds.password, user.password);
+      if (isvalid) {
+        return User.makePubUser(user);
+      }
+    }
     //if password correct, return user
     //if not throw err
+
     throw new UnauthorizedError("wrong user/password");
   }
   static async register(creds) {
